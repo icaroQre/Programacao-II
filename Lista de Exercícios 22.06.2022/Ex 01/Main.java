e97967e5411ec92014e2e69435e2e4f6c9a4c0cd
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-06-26 18:41:57 -0300

    Create Main.java

---
+++ Lista de Exercícios 22.06.2022/Ex 01/Main.java
@@ -0,0 +1,14 @@
+//Crie uma classe com métodos estáticos:
+//a. converterParaQuilometros( ): recebe a quantidade de milhas
+//(medida americana) e converte para quilômetros
+//b. converterParaLitros( ): recebe a quantidade de galões (medida
+//americana) e converte para litros
+//Faça o método main( ) para mostrar como chamar e tratar o retorno dos
+//métodos.
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println(Metodos.converterParaQuilometros(3));
+    System.out.println(Metodos.converterParaLitros(5));
+  }
+}
