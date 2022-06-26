07c966b39d5bc5cb76debf973ae9724c4e264a40
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-06-26 15:56:00 -0300

    Update Main.java

--- Lista  de Exercícios 01/Ex 01/Main.java
+++ Lista  de Exercícios 01/Ex 01/Main.java
@@ -1,3 +1,10 @@
+//Crie uma classe chamada Lampada. A classe deve ter o atributo (campo) acesa.
+//A classe deve ter os métodos:
+//● acender( ) (muda o campo acesa para verdadeiro)
+//● apagar( ) (muda o campo acesa para falso)
+//Crie um método chamado mostrarEstado( ) que retorna a mensagem “acesa” ou
+//“apagada” conforme o valor do campo acesa.
+  
 public class Main {
   public static void main(String[] args) {
 
