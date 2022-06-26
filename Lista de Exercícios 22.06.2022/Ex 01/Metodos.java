ccd59d227b420de4ff462d0598a9c0c468169008
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-06-26 18:42:28 -0300

    Create Metodos.java

---
+++ Lista de Exercícios 22.06.2022/Ex 01/Metodos.java
@@ -0,0 +1,15 @@
+public class Metodos 
+  {
+
+    static double converterParaQuilometros(double milhas){
+
+       return milhas * 1.609;
+      
+    }
+
+    static double converterParaLitros(double galoes){
+      
+    return galoes * 3.79;
+
+    }
+  }
