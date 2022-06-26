b9842ee92d209bdf80ee1e7866ff0f893758bd5d
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-06-26 18:40:27 -0300

    Update Main.java

--- Lista de Exercícios 22.06.2022/Ex 02/Main.java
+++ Lista de Exercícios 22.06.2022/Ex 02/Main.java
@@ -1,3 +1,11 @@
+//Crie uma classe chamada Proprietario com os atributos cpf, nome. Os
+//campos devem ser encapsulados (privados com gets e sets). Crie um construtor
+//vazio e um construtor que receba os valores. Crie uma classe chamada Casa com
+//os atributos matricula, endereco, valor e proprietario (do tipo
+//Proprietario). Encapsule os campos. Crie um construtor que inicialize os
+//campos adequadamente. Mostre como instanciar um objeto do tipo Casa e atribuir
+//valor aos campos do objeto.
+
 class Main {
   public static void main(String[] args) {
 
